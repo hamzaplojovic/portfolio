2d74dae6cd95ead14582c65e8cbd0bdc08812c61
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-02-24 21:51:36 +0100

    seo refactor

--- frontend/src/components/About/AboutUtils/Banner.tsx
+++ frontend/src/components/About/AboutUtils/Banner.tsx
@@ -1,12 +1,21 @@
-import {IconBrandGithubCopilot} from "@tabler/icons"
-import React from "react"
+import { IconBrandGithubCopilot } from "@tabler/icons";
+import React from "react";
 
 export const Banner = () => {
     return (
         <div className="about-image-wrapper" data-aos="fade-up">
-            <a href="https://github.com/hamzaplojovic" target="_blank" rel='noopener noreferrer'>
-                <IconBrandGithubCopilot size={550} stroke={2} className="about-image" />
+            <a
+                href="https://github.com/hamzaplojovic"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Link To Github"
+            >
+                <IconBrandGithubCopilot
+                    size={550}
+                    stroke={2}
+                    className="about-image"
+                />
             </a>
         </div>
-    )
-}
+    );
+};
--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -1,15 +1,25 @@
-import React from "react"
-import { IconChevronUp } from "@tabler/icons"
-import "../index.scss"
+import React from "react";
+import { IconChevronUp } from "@tabler/icons";
+import "../index.scss";
 
 export const ScrollToTop = () => {
-    return <button className="scroll-to-top" type="button" onClick={()=>
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
-    }>
-        <IconChevronUp size={50} stroke={0.5} className="scroll-to-top-icon"/>
-    </button>
-    
-}
+    return (
+        <button
+            className="scroll-to-top"
+            type="button"
+            aria-label="Scroll to top"
+            onClick={() =>
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                })
+            }
+        >
+            <IconChevronUp
+                size={50}
+                stroke={0.5}
+                className="scroll-to-top-icon"
+            />
+        </button>
+    );
+};
